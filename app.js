ed974c037df329e7f0cd995c9f2442a26673ca92
sarafotros <sarafotros@gmail.com>
2020-07-09 00:15:08 +0100

    styling + need to be added

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const sounds = document.querySelectorAll(".sound");
 const pads = document.querySelectorAll(".pads div");
 
-pads.forEach((pads, index) => {
-  pads.addEventListener("click", () => {
+pads.forEach((pad, index) => {
+  pad.addEventListener("click", () => {
     sounds[index].currentTime = 0;
     sounds[index].play();
+    pad.classList.add("play");
   });
 });
 
 document.addEventListener("keydown", (e) => {
   //   console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-  //   console.log(audio);
+  const key = document.querySelector(`.pad[data-key='${e.keyCode}']`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
+  //
+  key.classList.add("play");
+  pads.forEach((pad) => {
+    pad.addEventListener("click");
+    console.log(pad);
+    pad.classList.add("play");
+  });
 });
